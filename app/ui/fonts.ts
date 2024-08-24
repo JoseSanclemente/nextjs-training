b2ce966604085a76a4a20290d00e012048bfa3b9
JoseSanclemente <darkarx06@gmail.com>
2024-08-24 11:44:58 -0500

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana } from "next/font/google";
+
+// Primary font
+export const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "700", "800"],
+});
+
+// Secondary font
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
